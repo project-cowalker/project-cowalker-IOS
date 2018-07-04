450ef9dbf2d4507dadb23600d9f8adc45fe415f3
yewon0665 <ywj1328@hanmail.net>
2018-07-05 04:21:27 +0900

    server connecting with recruiting team member

---
+++ Cowalker/1.Home/0.Project/-recruit/service/RecruitService.swift
@@ -0,0 +1,63 @@
+//
+//  RecruitService.swift
+//  Cowalker
+//
+//  Created by 조예원 on 2018. 7. 4..
+//  Copyright © 2018년 조예원. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import UIKit
+
+struct RecruitService:APIService{
+    static func recruitInit(position:String, start_date:String, end_date:String, number:Int, task:String, activity:String, reward:String, area:String, ability:String, career:String, preference:String, comment:String, question:[String], completion : @escaping (String)->Void) {
+        
+        let URL = url("/recruit")
+        //var headers = UserDefaults.standard.string(forKey: "token")
+        
+        let body: [String : Any] = [
+            "position" : position,
+            "start_date" : start_date,
+            "end_date" : end_date,
+            "number" : number,///////////
+            "task": task,
+            "activitiy" : activity,
+            "reward" : reward,
+            "area" : area,
+            "ability" : ability,
+            "career": career,
+            "preference": preference,
+            "comment" : comment,
+            "question" : question/////////////
+        ]
+        
+        let header: [String : String] = [
+            "authorization" : UserDefaults.standard.string(forKey: "token")!
+        ]
+        
+        Alamofire.request(URL, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseData() {res in
+            //print(0)
+            switch res.result{
+    
+            case .success:
+                if let value = res.result.value{
+                    print(1)
+                    //print()
+                    if let message = JSON(value)["message"].string {
+                     //   print("33333")
+                        completion(message)
+                        
+                    }
+                }
+                break
+            case .failure(let err):
+                print("fail")
+                print(err.localizedDescription)
+                break
+            }
+                }
+    }
+}
+
--- Cowalker/Login&Join/service/LoginService.swift
+++ Cowalker/Login&Join/service/LoginService.swift
@@ -10,9 +10,8 @@ import Alamofire
 import SwiftyJSON
 import UIKit
 
-
 struct LoginService:APIService{
-    static func logInit(email:String, password:String, completion : @escaping (String)->Void) {
+    static func logInit(email:String, password:String, completion : @escaping (Array<String>)->Void) {
         let URL = url("/signin")
         
         let body: [String : Any] = [
@@ -23,13 +22,18 @@ struct LoginService:APIService{
         Alamofire.request(URL, method: .post, parameters: body, encoding: JSONEncoding.default, headers: nil).responseData() {res in
             switch res.result{
             case .success:
-                print("성공1")
                 if let value = res.result.value{
-                    
+                   
+                    var arr = ["",""]
                     if let message = JSON(value)["message"].string {
-                        
-                        completion(message)
+                         //print(message)
+                        //completion(message)
+                        arr[0] = message
                     }
+                    if let token = JSON(value)["token"].string{
+                        arr[1] = token
+                    }
+                    completion(arr)
                 }
                 break
             case .failure(let err):
