991f8e57d36ec9f005fef82674ea1e91ae57db84
gooiljung <junggooil9191@gmail.com>
2018-07-06 05:45:02 +0900

    mypage View

---
+++ Cowalker/5.Profile/-mypage/IntroductionEditCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionEditCollectionViewCell.swift
+//  Cowalker
+//
+//  Created by 정보영 on 2018. 7. 6..
+//  Copyright © 2018년 조예원. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionEditCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var editingImage: UIImageView!
+    
+}
