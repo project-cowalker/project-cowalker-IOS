08d90c62b66a78edecba69e6571b3417bf54653d
gooiljung <junggooil9191@gmail.com>
2018-07-07 18:02:00 +0900

    connect project Detail

--- Cowalker/3.Create/service/CreateNewProjectService.swift
+++ Cowalker/3.Create/service/CreateNewProjectService.swift
@@ -89,61 +89,44 @@ struct CreateNewProjectService: APIService {
         }
         
     }
-    static func getProjectDeatil(){
+    
+    
+    static func getProjectDeatil(project_idx: String,completion: @escaping ([ProjectDetail]) -> Void){
+        let URL = url("/"+project_idx)
+        Alamofire.request(URL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseData() { res in
+            switch res.result {
+            case .success:
+                if let value = res.result.value {
+                    let decoder = JSONDecoder()
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSS'Z"
+                    decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                    do {
+                        let projectDetailData = try decoder.decode(ProjectDetailData.self, from: value)
+                        if projectDetailData.message == "success"{
+                            completion(projectDetailData.result)
+                        }
+                        
+                    }catch{
+                        
+                    }
+                }
+                break
+                
+                
+                
+                
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+            
+            
+        }
+        
         
     }
-//    let URL = url("/message")
-//
-//
-//    //        let header: [String : String] = [
-//    //            "authorization" : UserDefaults.standard.string(forKey: "token")!
-//    //        ]
-//
-//    let header = ["Authorization" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJpYXQiOjE1MzA2NzAxNTMsImV4cCI6MTUzMzI2MjE1M30.BdRb0yary7AY8_yi8MDRDXuXrW19QSqRJI-9Xin3SXs"]
-//
-//
-//
-//    Alamofire.request(URL, method: .get, parameters: nil , encoding: JSONEncoding.default, headers: header).responseData() { res in
-//    switch res.result {
-//    case .success:
-//    if let value = res.result.value {
-//    print(value)
-//    let decoder = JSONDecoder()
-//    let dateFormatter = DateFormatter()
-//    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSS'Z"
-//
-//    decoder.dateDecodingStrategy = .formatted(dateFormatter)
-//    print(1111111111)
-//    do{
-//
-//    print("do 를 들어옴")
-//    let messageData = try decoder.decode(MessageData.self, from: value)
-//    print(222222222)
-//    print(messageData)
-//    if messageData.message == "get message success"{
-//    print(messageData.message)
-//    completion(messageData.result)
-//
-//
-//    }else{
-//    print("access denied")
-//
-//    }
-//    }catch let err{
-//    print(err)
-//    print("catch")
-//    }
-//    }
-//
-//
-//
-//    break
-//    case .failure(let err):
-//    print(err.localizedDescription)
-//    break
-//    }
-//    }
-    
+
+
 }
 
 
