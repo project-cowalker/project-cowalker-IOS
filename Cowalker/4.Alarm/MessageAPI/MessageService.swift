1b4bcc4a50046f2bf433c6525fea57d32559f2d0
gooiljung <junggooil9191@gmail.com>
2018-07-07 21:25:59 +0900

    project detail 수정

--- Cowalker/4.Alarm/MessageAPI/MessageService.swift
+++ Cowalker/4.Alarm/MessageAPI/MessageService.swift
@@ -70,8 +70,8 @@ struct MessageService: APIService{
     
     // 상대방에게 쪽지 보내기 partner_idx 사용 쪽지함의 get 요청에서 partner_idx 를 받음
     // header 수정하기  지금은 Test 용 토큰
-    static func sendingMessageToPartner(partner_idx: String,contents: String, completion: @escaping () -> Void){
-        let URL = url("/"+partner_idx)
+    static func sendingMessageToPartner(partner_idx: Int,contents: String, completion: @escaping () -> Void){
+        let URL = url("/"+String(partner_idx))
         
         let body: [String: Any] = [
             "contents" : contents
@@ -99,39 +99,39 @@ struct MessageService: APIService{
         }
     }
     
+    //클릭해서 상대 1명과의 쪽지 가져오기 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    // header 수정하기 지금은 Test 용 토큰@@@@@@@@@@@@@@@@@@@@@ partner_idx로
     
+    static func individualMessageInit(partner_idx: Int, completion: @escaping([IndividualMessage]) -> Void){
+        let URL = url("/"+String(partner_idx))
+        
+//        let header: [String : String] = ["authorization" : UserDefaults.standard.string(forKey: "token")!
+        let header = ["Authorization" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJpYXQiOjE1MzA2NzAxNTMsImV4cCI6MTUzMzI2MjE1M30.BdRb0yary7AY8_yi8MDRDXuXrW19QSqRJI-9Xin3SXs"]
+        
+        Alamofire.request(URL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header).responseData() { res in
+            switch res.result {
+            case .success:
+                if let value = res.result.value {
+                    let decoder = JSONDecoder()
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSS'Z"
+                    decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                    do {
+                        let individualMessageData = try decoder.decode(IndividualMessageData.self, from: value)
+                        if individualMessageData.message == "get message success"{
+                            completion(individualMessageData.result)
+                        }
+                    }catch let err{
+                        print(err)
+                        
+                    }
+                }
+                break
+            case .failure(let err):
+                print(err.localizedDescription)
+                break
+            }
+        }
+    }
 }
-//static func saveContent(title: String, content: String, password: String, completion: @escaping () ->Void) {
-//    let URL = url("/board")
-//    let userdefault = UserDefaults.standard
-//    guard let nickName = userdefault.string(forKey: "nickName") else {return}
-//    let body: [String: Any] = [
-//        "user_id" : nickName,
-//        "board_title" : title,
-//        "board_content" : content,
-//        "board_pw" : password,
-//        ]
-//
-//    Alamofire.request(URL, method: .post, parameters: body, encoding: JSONEncoding.default, headers: nil).responseData() { res in
-//        switch res.result {
-//        case .success:
-//            //////// 직접 구현해보세요. ////////
-//
-//            if let value = res.result.value {
-//                let message = JSON(value)["message"].string
-//                if message == "Successful Register Board Data"{
-//                    completion()
-//                }
-//            }
-//
-//
-//
-//            ////////
-//            break
-//        case .failure(let err):
-//            print(err.localizedDescription)
-//            break
-//        }
-//    }
-//}
 
---
+++ Cowalker/4.Alarm/messageModel/IndividualMessage.swift
@@ -0,0 +1,22 @@
+//
+//  IndividualMessage.swift
+//  Cowalker
+//
+//  Created by 정보영 on 2018. 7. 7..
+//  Copyright © 2018년 조예원. All rights reserved.
+//
+
+import Foundation
+
+struct IndividualMessage: Codable {
+    let to_user_name: String?
+    let from_user_name: String?
+    let contents: String?
+    let create_at: Date
+}
+
+//"to_user_name": "김가영",
+//"from_user_name": "나",
+//"contents": "저기요",
+//"create_at": "2018-07-06T01:26:07.586Z"
+
