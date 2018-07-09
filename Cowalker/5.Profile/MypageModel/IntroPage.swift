fd39e7aea5bc8bb76d001eb07a88c177a6afa51b
gooiljung <junggooil9191@gmail.com>
2018-07-09 20:36:54 +0900

    myPage Intro

---
+++ Cowalker/5.Profile/MypageModel/IntroPage.swift
@@ -0,0 +1,17 @@
+//
+//  IntroPage.swift
+//  Cowalker
+//
+//  Created by 정보영 on 2018. 7. 9..
+//  Copyright © 2018년 조예원. All rights reserved.
+//
+
+import Foundation
+
+struct IntroPage: Codable {
+
+    let intro_idx: String?
+    let intro_contents: String?
+    let intro_img_url: [String]?
+
+}
--- Cowalker/5.Profile/MypageModel/ParticipatedProject.swift
+++ Cowalker/5.Profile/MypageModel/ParticipatedProject.swift
@@ -7,18 +7,7 @@
 //
 
 import Foundation
-//"img_url": [
-//"https://project-cowalker.s3.ap-northeast-2.amazonaws.com/1530871591745.jpg"
-//],
-//"_id": "5b3f3f28a989031a3ef84e3c",
-//"title": "dd",
-//"summary": "dd",
-//"area": "강원도",
-//"department": "컨텐츠",
-//"aim": "사이드 프로젝트",
-//"explain": "ss",
-//"user_idx": 3,
-//"create_at": "2018-07-06T10:06:32.218Z"
+
 struct ParticipatedProject: Codable {
     let img_url: [String]?
     let _id: String?
