739640e7af7627c4f66376f6dc82655b68fe5a4b
yewon0665 <ywj1328@hanmail.net>
2018-07-10 17:16:43 +0900

    server ¿¬°

--- Cowalker/Login&Join/1.Home/HomeViewController.swift
+++ Cowalker/Login&Join/1.Home/HomeViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Kingfisher
+
 
 class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
@@ -70,30 +72,53 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return 0
     }
     // 셀 클릭시 다음 화면으로 이동
-    var projectDetails: [ProjectDetail] = [ProjectDetail]()
+   var projectDetails: [ProjectDetail] = [ProjectDetail]()
     var recruitLists: [RecruitList] = [RecruitList]()
+    
     let temp: String = ""
     let tempToken = "5b3dd2387172d402215033d2"
+    var tempForProfile = UIImageView()
+    var tempForBackground = UIImageView()
+    
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         if collectionView == listCollectionView{
             let secondVC = UIStoryboard(name: "Project", bundle:nil ).instantiateViewController(withIdentifier: "ProjectIntroViewController") as! ProjectIntroViewController
             // 서버 통신 1
             CreateNewProjectService.getProjectDeatil(project_idx: tempToken ){ (projectDetails, temp) in
-                self.projectDetails = projectDetails
-                secondVC.titleLabel.text = self.projectDetails[0].title
-                //
-                secondVC.tempProjectDetails = projectDetails
+                secondVC.titleLabel.text = projectDetails[0].title
+                secondVC.summaryLabel.text = projectDetails[0].summary
+                secondVC.aimLabel.text = projectDetails[0].aim
+                secondVC.areaLabel.text = projectDetails[0].area
+                secondVC.departmentLabel.text = projectDetails[0].department
+                secondVC.explainLabel.text = projectDetails[0].explain
+                secondVC.nameLabel.text = projectDetails[0].project_user_name
+                //이미지 처리
+                
+                //이미지뷰 처리
             }
+            
             // 서버 통신 2 - 모집 관련 호출 // 통신 값을 다음 화면의 컬랙션에 출력해야된다.
             RecruitService.recruitList(add: tempToken){ (recruitLists) in
-                self.recruitLists = recruitLists
-                secondVC.titleLabel.text = self.recruitLists[0].position
-                //
-                secondVC.tempRecruitLists = recruitLists
+                
+               // self.recruitLists = recruitLists
+                //secondVC.titleLabel.text = self.recruitLists[0].position
+              //  secondVC.tempRecruitLists = recruitLists
+                //print(123123123)
+               // print(secondVC.tempRecruitLists.count)
             }
             self.navigationController?.pushViewController(secondVC, animated: true) // 화면전환
         }
     }
+    
+    /*
+    func imageInit(){
+        
+        self.tempForProfile.kf.setImage(with: URL(string: gsno( "https://project-cowalker.s3.ap-northeast-2.amazonaws.com/1530983561891.png")),placeholder: UIImage())
+        let secondVC = UIStoryboard(name: "Project", bundle:nil ).instantiateViewController(withIdentifier: "ProjectIntroViewController") as! ProjectIntroViewController
+        
+        secondVC.profileImg.setBackgroundImage(self.tempForProfile.image, for: UIControlState.normal)
+        
+    }*/
 }
 
 
